2ee47c3d3570dc4088518608010e0571ae01a4e3
xiamu <myjyixi@163.com>
2021-01-16 18:11:39 +0800

    feat(utils): add util importClassesFromDirs

---
+++ src/utils/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: xiamu
+ * @Date: 2021-01-16 17:33:25
+ * @LastEditors: xiamu
+ * @LastEditTime: 2021-01-16 17:33:36
+ * @FilePath: /refactor-op-handler-demo/src/utils/index.ts
+ * @Description: 
+ */
+
+export * from './importClassesFromDirs';
